dbff5d461591b37e4250ae5f8f33770b700e8bb0
vagnermaltauro <vagnermaltauro@gmail.com>
2024-03-23 18:15:44 -0300

    feat: add new CRUD routes for openings in API v1

--- router/routes.go
+++ router/routes.go
@@ -15,4 +15,32 @@ func initializeRoutes(router *gin.Engine) {
       })
     })
   }
+  {
+    v1.POST("/opening", func(ctx *gin.Context) {
+      ctx.JSON(http.StatusOK, gin.H{
+        "message": "POST /api/v1/opening",
+      })
+    })
+  }
+  {
+    v1.DELETE("/opening", func(ctx *gin.Context) {
+      ctx.JSON(http.StatusOK, gin.H{
+        "message": "DELETE /api/v1/opening",
+      })
+    })
+  }
+  {
+    v1.PUT("/opening", func(ctx *gin.Context) {
+      ctx.JSON(http.StatusOK, gin.H{
+        "message": "PUT /api/v1/opening",
+      })
+    })
+  }
+  {
+    v1.GET("/openings", func(ctx *gin.Context) {
+      ctx.JSON(http.StatusOK, gin.H{
+        "message": "LIST /api/v1/opening",
+      })
+    })
+  }
 }
