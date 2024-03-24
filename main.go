3cd40f064c26823924bde0916eac6d757700c88e
vagnermaltauro <vagnermaltauro@gmail.com>
2024-03-24 11:03:06 -0300

    feat: add logger initialization and error handling

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"github.com/vagnermaltauro/gopportunities/config"
 	"github.com/vagnermaltauro/gopportunities/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization failed: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
