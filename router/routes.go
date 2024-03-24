8e63b82b131cdeb5cca3b084a83106d8c9706587
vagnermaltauro <vagnermaltauro@gmail.com>
2024-03-24 09:35:44 -0300

    refactor: route handling in routes initialization

--- router/routes.go
+++ router/routes.go
@@ -9,17 +9,9 @@ func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/opening", handler.GetOpeningHandler)
-	}
-	{
 		v1.POST("/opening", handler.CreateOpeningHandler)
-	}
-	{
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
-	}
-	{
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
-	}
-	{
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
