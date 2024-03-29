a16b7cb3e05b1a777f48c83b05a65bb77f504519
vagnermaltauro <vagnermaltauro@gmail.com>
2024-03-29 13:19:16 -0300

    refactor: delete opening handler with query validation

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,32 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/vagnermaltauro/gopportunities/schemas"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "DELETE /api/v1/opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		ctx.JSON(http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, fmt.Sprintf("Opening with id %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, fmt.Sprintf("Failed to delete opening with id %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "Delete opening", opening)
 }
