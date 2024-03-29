989f7dad4c4a833db629dba728c57c7be3629ccf
vagnermaltauro <vagnermaltauro@gmail.com>
2024-03-29 11:02:07 -0300

    feat: add CreateOpeningRequest struct with validation logic

---
+++ handler/request.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"fmt"
+)
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type %s) is required", name, typ)
+}
+
+type CreateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   string `json:"salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" {
+		return errParamIsRequired("role", "string")
+	}
+
+	if r.Company == "" {
+		return errParamIsRequired("company", "string")
+	}
+
+	if r.Location == "" {
+		return errParamIsRequired("location", "string")
+	}
+
+	if r.Remote == nil {
+		return errParamIsRequired("remote", "bool")
+	}
+
+	if r.Link == "" {
+		return errParamIsRequired("link", "string")
+	}
+
+	if r.Salary == "" {
+		return errParamIsRequired("salary", "string")
+	}
+
+	return nil
+}
